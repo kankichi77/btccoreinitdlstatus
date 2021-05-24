e50761d0e9e65ebd573d30ca4c2072f8e2fb2259
kan <kan@github>
2021-05-24 12:01:13 +0800

    improvements

--- status.py
+++ status.py
@@ -3,10 +3,14 @@ os.system("clear")
 bc_current = int(subprocess.check_output(["bitcoin-cli", "getblockcount"]).decode("utf-8"))
 bc_height = int(subprocess.check_output(["wget", "-O", "-", "https://blockchain.info/q/getblockcount"],stderr=subprocess.DEVNULL).decode("utf-8"))
 bc_diff = bc_height - bc_current
+diff_percent = bc_current / bc_height * 100
 
 print("Current Block Height: %d" % bc_height)
 print("Current Block: %d" % bc_current)
-print("Diff: %d" % bc_diff)
+if (bc_diff == 0):
+    print("Blockchain is in sync!")
+else :
+    print("Progress {:.2f}%, {} blocks remaining".format(diff_percent, bc_diff))
 
 #time.sleep(1)
 #inp = input("in: ")
